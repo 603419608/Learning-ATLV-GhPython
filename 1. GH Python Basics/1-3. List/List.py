25fc5c9b96669f1f826da907b11d3fc81d42a567
603419608 <603419608@users.noreply.github.com>
2023-06-25 17:36:27 +0800

    List

---
+++ 1. GH Python Basics/1-3. List/List.py
@@ -0,0 +1,9 @@
+﻿import rhinoscriptsyntax as rs
+
+a = [] # set an empty list(创建一个空列表存储生成的点)
+for i in range(10):
+    p = rs.AddPoint(i,0,0)
+    a.append(p)
+
+#List Comprehensions (列表解析)
+b = [rs.AddPoint(0,0,i) for i in range(10)]
