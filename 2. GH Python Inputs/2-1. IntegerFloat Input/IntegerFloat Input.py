f9319d703c59912fa11833ff9a773b32feeb500b
603419608 <603419608@users.noreply.github.com>
2023-06-25 19:22:54 +0800

    Integer/Float Input

---
+++ 2. GH Python Inputs/2-1. IntegerFloat Input/IntegerFloat Input.py
@@ -0,0 +1,11 @@
+﻿import Rhino.Geometry as rg
+import math
+
+pts = []
+for i in range(u_count):
+    for j in range(v_count):
+        p = rg.Point3d(i, j, math.sin(i*u) * math.sin(j*u) * z)
+        pts.append(p)
+#Creating a grid of points using list comprehension.
+#pts2 = [rg.Point3d(i, j, math.sin(i*u) * math.sin(j*u) * z)  for i in range(u_count) for j in range(v_count)]
+srf = rg.NurbsSurface.CreateFromPoints(pts,u_count,v_count,3,3)
