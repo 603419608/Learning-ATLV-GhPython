79283bc3ff22c3b3094a58f59ae7539e8590fa85
603419608 <603419608@users.noreply.github.com>
2023-06-25 18:35:09 +0800

    Random Number

---
+++ 1. GH Python Basics/1-7 Random Number/Random Number.py
@@ -0,0 +1,13 @@
+﻿import Rhino.Geometry as rg
+import random
+import math
+
+a = []
+for i in range(500):
+    x = math.cos(i*0.2) * i*0.1
+    y = math.sin(i*0.2) * i*0.08
+    if(i<200):
+        z = 0
+    else:
+        z = random.random() * (i-200)* 0.02
+    a.append(rg.Point3d(x,y,z))
