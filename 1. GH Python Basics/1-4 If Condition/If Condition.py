f3b915ba9d5c2dfcbd3fd01ad0de6759ab1e8a8a
603419608 <603419608@users.noreply.github.com>
2023-06-25 17:56:47 +0800

    If Condition

---
+++ 1. GH Python Basics/1-4 If Condition/If Condition.py
@@ -0,0 +1,11 @@
+﻿import rhinoscriptsyntax as rs
+
+a = []
+for i in range(10):
+    if(i%2 == 0):
+        p = rs.AddPoint(i,0,0)
+    else:
+        p = rs.AddPoint(i,0,5)
+    a.append(p)
+
+pl = rs.AddPolyline(a)
---
+++ 1. GH Python Basics/1-5 If Condition2/If Condition (2).py
@@ -0,0 +1,13 @@
+﻿import rhinoscriptsyntax as rs
+
+a = []
+for i in range(10):
+    if(i < 5):
+        p = rs.AddPoint(i,0,0)
+    elif(i < 8):
+        p = rs.AddPoint(i,0,6)
+    else:
+        p = rs.AddPoint(i,0,2)
+    a.append(p)
+
+pl = rs.AddPolyline(a)
