3662a3de6b242637a12bbca97cb0ec9ee5af9ba1
603419608 <603419608@users.noreply.github.com>
2023-06-25 22:31:20 +0800

    Point Input(2)

---
+++ 2. GH Python Inputs/2-3. Point Input (2)/2-3. Point Input (2).py
@@ -0,0 +1,15 @@
+﻿import rhinoscriptsyntax as rs
+import Rhino.Geometry as rg     
+
+a = []
+b = []
+for pt in pts:
+    if( pt.Y % 10 > 0 and pt.Y % 10 < 2):
+        rect = rs.AddRectangle(rg.Plane.WorldXY, 1.5, 0.4)
+        rs.MoveObject(rect, pt)
+        rs.MoveObject(rect, rg.Vector3d(-0.75,-0.2,0))
+        a.append(rect)
+    else:
+        dist = rs.Distance(pt, attr)
+        circle = rs.AddCircle(pt, 0.02*dist)
+        b.append(circle)
---
+++ 2. GH Python Inputs/2-3. Point Input (2)/Interference case of points.py
@@ -0,0 +1,23 @@
+﻿import Rhino.Geometry as rg
+import sys
+
+center_pts = []
+dists = []
+for cell in cells:
+    #Finding the center point of a polyline(查找中心点)
+    center = cell.CenterPoint()
+    center_pts.append(center)
+    
+    #Calculating the distance from the nearest interference point to the center point.
+    #(求中心点到最近干扰点的距离)
+    min_distance =sys.float_info.max
+    for pt in pts:
+        distance = pt.DistanceToSquared(center)
+        if distance < min_distance :
+            min_distance = distance
+    dists.append(min_distance)
+
+#Mapping the nearest distance to a range of 0.2 to 0.85(把最近距离映射到0.2 to 0.85之间)
+domain = rg.Interval(0.85,0.2)
+dists_domain = rg.Interval(min(dists),max(dists))
+circles = [rg.Circle(pt,domain.ParameterAt(dists_domain.NormalizedParameterAt(dists[i]))) for i,pt in enumerate(center_pts)]
