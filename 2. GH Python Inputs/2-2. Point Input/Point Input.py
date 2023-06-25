f8df3b0fff95dae95ab67f5bcfb6a2066d5c4f09
603419608 <603419608@users.noreply.github.com>
2023-06-25 19:37:32 +0800

    Point Input

---
+++ 2. GH Python Inputs/2-2. Point Input/Point Input.py
@@ -0,0 +1,18 @@
+﻿import rhinoscriptsyntax as rs
+import math
+
+a = []
+b = []
+c = []
+for pt in pts:
+    if(pt.X % 3 < 0.5):
+        a.append(pt)
+    elif( math.sqrt(abs(pt.Y)) %0.5 < 0.1):
+        b.append(pt)
+        """
+        The math.sqrt function does not accept negative numbers,
+        so it needs to be preceded by the abs function to take the absolute value.
+        math.sqrt函数不能输入负数，所以需要加一个abs函数取绝对值.
+        """
+    else:
+        c.append(pt)
