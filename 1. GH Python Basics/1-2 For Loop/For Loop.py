efb90228df44f4cfee847161b9bd878c5723eb7f
603419608 <603419608@users.noreply.github.com>
2023-06-25 17:21:10 +0800

    For Loop

---
+++ 1. GH Python Basics/1-2 For Loop/For Loop.py
@@ -0,0 +1,29 @@
+﻿import rhinoscriptsyntax as rs
+
+print("================= For loop =================")
+#For loop
+for i in range(10):
+    print(i)
+
+print("================= While loop =================")
+#While loop
+num = 0
+a = []
+while (num<10):
+    a.append(rs.AddPoint(num,0,0))
+    print num
+    num += 1
+
+print("================= continue =================")
+#continue
+for i in range(10):
+    if(i%2==0):
+        continue
+    print i
+
+print("================= break =================")
+#break
+for i in range(10):
+    print i
+    if(i%2==0):
+        break
