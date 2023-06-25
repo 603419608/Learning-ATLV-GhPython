15cd9cd86dcc7d4eead3c0f53d5969046afcf42d
603419608 <603419608@users.noreply.github.com>
2023-06-25 17:00:45 +0800

    First Code

---
+++ 1. GH Python Basics/1-1 First Code/First Code.py
@@ -0,0 +1,21 @@
+﻿#importing modules. (导入模块的3种方式)
+import rhinoscriptsyntax as rs
+from Rhino.Geometry import Point3d
+ma = __import__("math")
+
+
+
+text = "hello world"
+print text
+
+# comment (单行注释#)
+a = rs.AddPoint(0,0,0)
+
+"""
+(多行注释""""""")
+Creating a point using Rhinocommon.(使用Rhinocommon创建一个点)
+"""
+b = Point3d(10,10,10)
+
+
+c = x + y
