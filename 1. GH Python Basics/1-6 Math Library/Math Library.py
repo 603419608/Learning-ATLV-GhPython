846e413a004014000ee6bc81d243e64e9fb9652c
603419608 <603419608@users.noreply.github.com>
2023-06-25 18:05:47 +0800

    Math Library

---
+++ 1. GH Python Basics/1-6 Math Library/Math Library.py
@@ -0,0 +1,15 @@
+﻿import rhinoscriptsyntax as rs
+import math
+
+a = []
+for i in range(500):
+    x = math.cos(i*0.1) * i*0.1
+    y = math.sin(i*0.1) * i*0.08
+    if(i < 200):
+        z = i*0.05
+    else:
+        z = 20-i*0.05
+    p = rs.AddPoint(x,y,z)
+    a.append(p)
+
+b = rs.AddCurve(a)
